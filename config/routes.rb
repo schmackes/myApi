aac2cec7375a2b5dda3dc94202d25a67ede9d292
markus <mr.markus.kaiser@gmail.com>
2012-12-10 19:24:36 +0100

    first request to api returns as json

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,10 @@ MyApi::Application.routes.draw do
   #     resources :products
   #   end
 
+  namespace :api do
+    resources :destination_matrix
+  end
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => 'home#index'
