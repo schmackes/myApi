e2d1f23c99d3dc481fcdb022097d74ccd9c82c61
markus <mr.markus.kaiser@gmail.com>
2012-12-10 17:36:58 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApi::Application.config.secret_token = '13a6d3e0ef63aa18748ba7fdd7a95c0891ec2fd2d5f46dadb95cf3330a0fb6d988561b3bfce70bb4dcb706cd28cb5cbfab68c7df181154f799d8c94f73a97159'
