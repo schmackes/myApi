6879ddf216e6637c82fc372397403fba70e8f7dc
markus <mr.markus.kaiser@gmail.com>
2012-12-19 23:46:26 +0100

    escaping special signs in url

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,29 +4,47 @@ class Service < ActiveRecord::Base
  has_many :params
 
   def get_full_url
-#    full_url=self.url
+    full_url=self.url
+
+    if self.params.size>0
+      full_url+="?"
+
+      self.params.each do |param|
+        full_url+=encode(param.name)+"="+encode(param.value)+"&"
+      end
+      full_url=full_url[0..-2] #remove last &
+    end
+
+    return full_url
+  end
+
+
 #
-#   if self.params.size>0
-#     full_url+="?"
 #
-#     self.params.each do |param|
-#      full_url+=param.name+"="+param.value+"&"
-#     end
+#  query_string=""
 #
-#     full_url=full_url[0..-2] #remove last &
-#   end
 #
-#     puts "Full URL:"+ full_url
-#     return full_url
+#  if self.params.size>0
+#
+#
+#    #use only name,value of param; encode special signs
+#    key_value_params = Hash[self.params.map{|p| [encode(p.name), encode(p.value)]}]
+#
+#    #generate query string (e.g. searchFor=markus&filter=true)
+#    #query_string=key_value_params.to_query
+#    #URI.encode_www_form(key_value_params)
+#    #puts query_string
+#
+#  else
+#
 #  end
 #
-
-    key_value_params = Hash[self.params.map{|p| [p.name, p.value]}]
-    query_string=URI.encode_www_form(key_value_params)
-
-    #url encoding (e.g. space to %
-    encoded_query_string =URI.encode_www_form_component(query_string.to_s)
-    full_url=URI.join(self.url, encoded_query_string).to_s
-
+#  #join with url (e.g. http://www.example.com/search
+#  full_url=self.urlquery_string)
+#
 #end
+#
+  def encode(string)
+    URI.escape(string)
+  end
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,21 +1,50 @@
 require 'spec_helper'
 
 describe Service do
-  it "should return a valid url in case of no params" do
-    param1=Param.new(:name=>"searchFor", :value=>"markus")
-    param2=Param.new(:name=>"filter", :value=>"true")
-    service=Service.new(:name=>"dummyURL", :description=>"a dummy url", :url=>"http://www.dummy.de/search")
-    service.params << param1
-    service.params << param2
-    param1.save
-    service.save
-
-    puts "final url:"+service.get_full_url
+  before(:each) do
+    service_name, service_desc, @service_url="dummyService","this is a dummy desc","http://www.dummy.de/search"
+    @service=Service.new(:name=>service_name, :description=>service_desc, :url=>@service_url)
   end
 
   it "should return a valid url in case of params" do
+    name1,name2,value1,value2="searchFor","filter","markus","true"
+    param1=Param.new(:name=>name1, :value=>value1)
+    param2=Param.new(:name=>name2, :value=>value2)
+
+    @service.params << param1
+    @service.params << param2
+    param1.save
+    param2.save
+    @service.save
+
+    @service.get_full_url.should include(name1+"="+value1)
+    @service.get_full_url.should include(name2+"="+value2)
+
+    exp_pattern=Regexp.new("^"+@service_url+"?")
+    @service.get_full_url.should match(exp_pattern)
+  end
+
+  it "should return a valid url in case of no params" do
+    exp_pattern=Regexp.new("^"+@service_url+"$")
+    @service.get_full_url.should match(exp_pattern)
   end
 
   it "should return a valid url in case of params with special signs (e.g. space)" do
+    name1,name2,value1,value2="searchFor","filter","Dummy Road 17-1000 Berlin","100,200"
+    param1=Param.new(:name=>name1, :value=>value1)
+    param2=Param.new(:name=>name2, :value=>value2)
+
+    @service.params << param1
+    @service.params << param2
+    param1.save
+    param2.save
+    @service.save
+
+
+    @service.get_full_url.should include(name1+"=Dummy%20Road%2017-1000%20Berlin")
+    @service.get_full_url.should include(name2+"=100,200")
+
+    exp_pattern=Regexp.new("^"+@service_url+"?")
+    @service.get_full_url.should match(exp_pattern)
   end
 end
